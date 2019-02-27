0271a5f74f14d66b5d32055ae0328d98787e46fd
wyan111 <wyan111@student.monash.edu>
2019-02-27 13:06:21 +1100

    Create bin_diff.m

---
+++ Four channel phi/big phi/bin_diff.m
@@ -0,0 +1,15 @@
+function binary = bin_diff(twoDmatrix)
+%epoch*time points
+size_mat = size(twoDmatrix);
+eponum = size_mat(1);
+for iEpo = 1:eponum;
+    diff_mat = diff(twoDmatrix(iEpo,:));
+    size_diff = size(diff_mat);
+   for iTpoint = 1:size_diff(2)
+       if diff_mat(iTpoint) > 0
+           binary(iEpo,iTpoint) = 1;
+       else
+           binary(iEpo,iTpoint) = 0;
+       end
+   end
+end

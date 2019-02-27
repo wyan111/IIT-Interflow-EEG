0b897a33db5fa9c0d730bbcb401a4f414944c5d2
wyan111 <wyan111@student.monash.edu>
2019-02-27 14:00:14 +1100

    Create all-170802.m

---
+++ Put all souce localized data for 1 pair in one mat-file/all-170802.m
@@ -0,0 +1,27 @@
+iTM = [2,3,12,13,16,17];
+iRV = [6,7,10,11,14,15];
+iOC = [4,5,8,9,18,19];
+for i=1:length(iTM)
+ data = load(['Users/soliyan3261/Source Localized Data/170802/' '170802_HK1_T' num2str(iTM(i)) '_tm_PLAY_all.mat']);
+ alldata(1,i) = {data.Value};
+ end
+    for i=1:length(iTM)
+ data = load(['Users/soliyan3261/Source Localized Data/170802/' '170802_EK2_T' num2str(iTM(i)) '_tm_PLAY_all.mat']);
+ alldata(2,i) = {data.Value};
+    end
+    for i=1:length(iRV)
+ data = load(['Users/soliyan3261/Source Localized Data/170802/' '170802_HK1_T' num2str(iRV(i)) '_tmrv_PLAY_all.mat']);
+ alldata(3,i) = {data.Value};
+    end
+    for i=1:length(iRV)
+ data = load(['Users/soliyan3261/Source Localized Data/170802/' '170802_EK2_T' num2str(iRV(i)) '_tmrv_PLAY_all.mat']);
+ alldata(4,i) = {data.Value};
+    end
+    for i=1:length(iOC)
+ data = load(['Users/soliyan3261/Source Localized Data/170802/' '170802_HK1_T' num2str(iOC(i)) '_tmoc_PLAY_all.mat']);
+ alldata(5,i) = {data.Value};
+    end
+    for i=1:length(iOC)
+ data = load(['Users/soliyan3261/Source Localized Data/170802/' '170802_EK2_T' num2str(iOC(i)) '_tmoc_PLAY_all.mat']);
+ alldata(6,i) = {data.Value};
+   end

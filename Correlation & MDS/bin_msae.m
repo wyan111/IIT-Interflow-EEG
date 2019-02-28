3287bae78e0e67c70b13e9be1dbdd27c989c7ea0
wyan111 <wyan111@student.monash.edu>
2019-02-28 14:08:59 +1100

    Create bin_msae.m

---
+++ Correlation & MDS/bin_msae.m
@@ -0,0 +1,14 @@
+function binary = tobin(twoDmatrix) 
+%twoDmatrix : epoch x timepoint 
+[eponum,timelen] = size(twoDmatrix);
+mMatrix = median(twoDmatrix);
+for iTime = 1:timelen;
+   for iEpo = 1:eponum;
+        if twoDmatrix(iEpo,iTime) > mMatrix(iTime)
+        binary(iEpo,iTime) = 1;
+   else
+        binary(iEpo,iTime) = 0;
+   end
+   end
+end
+end
